2a7a5f4f41f3bc13956020a94eae438c98de41d2
laura cabos <lauracabos@gmail.com>
2024-06-11 11:17:56 +0200

    Implement brains show

--- app/controllers/brains_controller.rb
+++ app/controllers/brains_controller.rb
@@ -1,2 +1,17 @@
 class BrainsController < ApplicationController
+  before_action :set_brain
+
+  def index
+    @brains = Brain.all
+  end
+
+  def show
+    @brain = Brain.find(params[:id])
+  end
+
+  private
+
+  def set_brain
+    @brain = Brain.find(params[:id])
+  end
 end
