aa77ee45b9da9ece3eecb778c8e5430bcf7e9ed3
Johanna Botquin <johannabotquin@icloud.com>
2024-06-11 11:02:04 +0200

    debut seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,43 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "creating users..."
+
+cyril = User.create!(
+  first_name: "Cyril",
+  last_name: "d'Angelis",
+  pseudo: "Cyssou",
+  email: "cyrilleboss@gmail.com",
+  password: "cyril"
+)
+
+jeremy = User.create!(
+  first_name: "Jeremy",
+  last_name: "Ziani",
+  pseudo: "ZiaJe",
+  email: "jejedu13@hotmail.fr",
+  password: "jeremy"
+)
+
+romane = User.create!(
+  first_name: "Romane",
+  last_name: "Arvois",
+  pseudo: "Roro",
+  email: "romanechampagne@icloud.com",
+  password: "romane"
+)
+puts "end users"
+
+puts "creating brains..."
+
+Brain.create!(
+  name: "Cyril",
+  qi: 8,
+  languages: ["Français"],
+  specialties: ["Humor", "Artist Soul"],
+  price_per_day: 25,
+  user_id: cyril.id,
+  image:
+)
+puts "end brains"
