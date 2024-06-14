52980dc457caa7df0e18e419b6972de7d70825c7
laura cabos <lauracabos@gmail.com>
2024-06-14 15:35:28 +0200

    Filter almost done JS

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -6,49 +6,81 @@ export default class extends Controller {
   connect() {
 /*     console.log(this.qiTarget) */
 /*     console.log(this.languagesTargets)
-    console.log(this.languagesTargets[0].value); */
     // console.log(this.languagesTargets[0].checked);
 /*     console.log(this.specialtiesTargets)
     console.log(this.priceTarget) */
+/*     console.log(this.cardTargets);
+    const french = this.languagesTargets[0].value
+    console.log(this.cardTargets[1].getAttribute("data-languages").includes(french)); */
   }
 
   update(event) {
-    console.log("hello")
-    const filterQi = parseInt(this.qiTarget.value)
-    const filterPrice = this.priceTarget.value
+    event.preventDefault()
+
     const filterLanguages = []
     const filterSpecialties = []
-    this.languagesTargets.forEach(element => {
-      if (element.checked) {
-        languages.push(element.value)
-      }
-    });
-    this.specialtiesTargets.forEach(element => {
-      if (element.checked) {
-        specialties.push(element.value)
-      }
-    });
 
-    this.cardTargets.forEach(element => {
-      const price = parseInt(element.getAttribute("data-price"))
-      const qi = parseInt(element.getAttribute("data-qi"))
-      const languages = element.getAttribute("data-languages")
-      const specialties = element.getAttribute("data-specialties")
-      if (qi < filterQi ) {
-        element.style.display = "none"
-      } else {
-        element.style.display = ""
+    const filterQi = parseInt(this.qiTarget.value)
+    this.languagesTargets.forEach(language => {
+      if(language.checked) { filterLanguages.push(language.value) }
+     })
+    this.specialtiesTargets.forEach(specialty => {
+      if (specialty.checked) {
+        filterSpecialties.push(specialty.value)
       }
-
-      if (price > filterPrice ) {
-        element.style.display = "none"
-      } else {
-        element.style.display = ""
-      }
-
-      
-
     })
+    const filterPrice = parseInt(this.priceTarget.value)
+    const cards = this.cardTargets
 
+    if(filterQi) {
+      this.filterByQi(cards, filterQi)
+    }
+
+    if(filterLanguages.length > 0) {
+      this.filterByLanguage(cards, filterLanguages)
+    }
+
+    if(filterSpecialties) {
+      this.filterBySpecialty(cards, filterSpecialties)
+    }
+
+    if(filterPrice) {
+      this.filterByPrice(cards, filterPrice)
+    }
+  }
+
+  filterByPrice(cards, filterPrice) {
+    cards.forEach(card => {
+      if (card.dataset.price > filterPrice) {
+        card.classList.add("d-none")
+      } else {
+        card.classList.remove("d-none")
+      }
+    })
+  }
+
+  filterBySpecialty(cards, filterSpecialties) {
+    cards.forEach(card => {
+      if(!card.dataset.specialties.includes(filterSpecialties.join())) {
+        card.classList.add("d-none")
+      }
+    })
+  }
+
+  filterByLanguage(cards, filterLanguages) {
+    cards.forEach(card => {
+      if(!card.dataset.languages.includes(filterLanguages.join())) {
+        card.classList.add("d-none")
+      }
+    })
+  }
+  filterByQi(cards, filterQi) {
+    cards.forEach(card => {
+      if (card.dataset.qi < filterQi) {
+        card.classList.add("d-none")
+      } else {
+        card.classList.remove("d-none")
+      }
+    })
   }
 }
